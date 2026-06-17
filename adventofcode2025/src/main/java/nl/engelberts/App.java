38647789e28364741710a422e4cc3024cdca3c8e
u0369169071-stack <u0369169071@gmail.com>
2026-06-17 14:16:32 +0000

    Dag 6 afgerond

--- adventofcode2025/src/main/java/nl/engelberts/App.java
+++ adventofcode2025/src/main/java/nl/engelberts/App.java
@@ -5,8 +5,8 @@ package nl.engelberts;
  */
 public class App {
     public static void main(String[] args) {
-        Day06 current_day = new Day06("adventofcode2025/data/test06.txt");
-        // System.out.println("Part A: " + current_day.resultA());
-        // System.out.println("Part B: " + current_day.resultB());
+        Day06 current_day = new Day06("data/real06.txt");
+        System.out.println("Part A: " + current_day.resultA());
+        System.out.println("Part B: " + current_day.resultB());
     }
 }
--- adventofcode2025/src/main/java/nl/engelberts/Day06.java
+++ adventofcode2025/src/main/java/nl/engelberts/Day06.java
@@ -8,15 +8,19 @@ import java.util.Arrays;
  */
 
 public class Day06 {
+    ArrayList<int[]> numbersA;
+    ArrayList<String> operators;
+    ArrayList<int[]> numbersB;
+    String[] lines;
 
     public Day06(String filename) {
         final String[] possibleOperators = {"*", "+", "*", "+"};
-
+    
         // Parse input data
         Parser parser = new Parser(filename);
-        String[] lines = parser.getDataLines();
-        ArrayList<int[]> numbersA = new ArrayList<int[]>();
-        ArrayList<String> operators = new ArrayList<String>(); 
+        lines = parser.getDataLines();
+        numbersA = new ArrayList<int[]>();
+        operators = new ArrayList<String>(); 
         for (String line : lines) {
             ArrayList<Integer> row = new ArrayList<Integer>();
             for (String word : line.split("\s+")) {
@@ -33,13 +37,67 @@ public class Day06 {
             }
             numbersA.add(row.stream().mapToInt(i -> i).toArray());
         }
+        // Remove the last (empty) line
+        numbersA.remove(numbersA.size() - 1);
+    }
 
-        // Calculate the result
-        for (int[] row : numbersA) {
-            for (int number : row) {
-                System.out.print(number + " ");
+    // Part A: Calculate the product and sum of numbers based on the operators
+    public long resultA() {
+        long totalSum = 0;
+        for (int i = 0; i < operators.size(); i++) {
+            long localProduct = 1;
+            long localSum = 0;
+
+            for (int j = 0; j < numbersA.size(); j++) {
+                int number = numbersA.get(j)[i];
+                String operator = operators.get(i);
+                if (operator.equals("*")) {
+                    localProduct *= number;
+                } else if (operator.equals("+")) {
+                    localSum += number;
+                }
             }
-            System.out.println();
+
+            if (localProduct > 1) {
+                totalSum += localProduct;
+            } else {
+                totalSum += localSum;
+            }
         }
+        return totalSum;
+    }
+
+    public long resultB() {
+        String operator = "";
+        long totalSum = 0;
+        long total = 0;
+        for (int i = 0; i < lines[0].length(); i++) {
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < lines.length; j++) {
+                char c = lines[j].charAt(i);
+                if (c == ' ') {
+                    continue;
+                } else if (c == '*') {
+                    operator = "*";
+                    totalSum += total;
+                    total = 1;
+                } else if (c == '+') {
+                    operator = "+";
+                    totalSum += total;
+                    total = 0;
+                } else {
+                    sb.append(c);
+                }
+            }
+            if (sb.length() > 0) {
+                if (operator.equals("*")) {
+                    total *= Long.parseLong(sb.toString());
+                } else if (operator.equals("+")) {
+                    total += Long.parseLong(sb.toString());
+                }
+            }
+        }
+        totalSum += total;
+        return totalSum;
     }
 }
